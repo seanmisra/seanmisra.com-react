b4a2e3dfbb720a5fe3d456ea35f3daabf9c7f643
Sean Misra <seanmisra30@gmail.com>
2023-01-18 17:15:25 -0500

    Profile pic and profile description

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { Image } from 'antd';
+import { Card, Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      <div class="content-container">
+
+        <div class="image-container">
+          <Image 
+            width={250}
+            src={require("./resources/website_profile_photo.jpg")}
+          />
+        </div>
+
+        <div class="bio-container">
+          <Card
+            style={{ width: "76vw", height: 256.5, fontSize: 26}} 
+   
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+            >
+            <Meta
+              avatar={<Avatar size="large" src={require("./resources/avatar.jpg")} />}
+              title="Hello!"
+              description={"Hi, my name is Sean and I'm a software engineer. I am passionate about web engineering! Please feel free to check out some of my projects. My hobbies include travel, movies, science-fiction, and biking."}
+            />
+           </Card>
+        </div>
+
+      </div>
+      
+
+
     </div>
   );
 }

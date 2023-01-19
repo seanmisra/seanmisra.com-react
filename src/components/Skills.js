8462c0c364c139e2dc15019bcc18cd963846f4c2
Sean Misra <seanmisra30@gmail.com>
2023-01-18 22:37:41 -0500

    Skills timeline outline

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,21 +5,71 @@ const Skills = () => {
     return(
         <>
             <div class="timeline-wrapper">
-                <hr id="timeline-line"></hr>
                 
-                <div class="timeline-bubble">
-                </div>
-                <span class="timeline-date">
-                    2015
+                <div class="timeline-bubbles-wrapper">
+                    <div id="time-bubble-one-wrapper">
+                        <div id="timeline-bubble-one" class="timeline-bubble">
+                            <span class="timeline-year" id="timeline-year-one">
+                                2015
+                            </span>
+                        </div>
+                    </div>
 
-                    {/* 2015: Wordpress, HTML, CSS
+                    <div id="time-bubble-two-wrapper">
+                        <div id="timeline-bubble-two" class="timeline-bubble">
+                            <span class="timeline-year" id="timeline-year-two">
+                                16
+                            </span>
+                        </div>
+                    </div>
+
+                    <div id="time-bubble-three-wrapper">
+                        <div id="timeline-bubble-three" class="timeline-bubble">
+                            <span class="timeline-year" id="timeline-year-three">
+                                17 
+                            </span>
+                        </div>
+                    </div>
+
+                    <div id="time-bubble-four-wrapper">
+                        <div id="timeline-bubble-four" class="timeline-bubble">
+                            <span class="timeline-year" id="timeline-year-four">
+                                18
+                            </span>
+                        </div>
+                    </div>
+
+                    <div id="time-bubble-five-wrapper">
+                        <div id="timeline-bubble-five" class="timeline-bubble">
+                            <span class="timeline-year" id="timeline-year-five">
+                                20
+                            </span>
+                        </div>
+                    </div>
+
+                    <div id="time-bubble-six-wrapper">
+                        <div id="timeline-bubble-six" class="timeline-bubble">
+                            <span class="timeline-year" id="timeline-year-six">
+                                22 
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+                <hr id="timeline-line"></hr>
+
+
+
+
+
+
+                                    {/* 2015: Wordpress, HTML, CSS
                     2016: C 
                     2017: PHP
                     2018: Angular, Java 
                     2020: Spring, Scala, Akka? 
                     2021: React */}
 
-                </span>
             </div>
         </>
     )

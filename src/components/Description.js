81b1c56ee3018ceee7ea903847c0e7244905b9ea
Sean Misra <seanmisra30@gmail.com>
2023-01-20 08:10:50 -0500

    Description responsivness

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FileWordOutlined } from '@ant-design/icons';
 import resume from '../resources/Sean_Misra_Resume_Aug_2022.docx'
 import { saveAs } from 'file-saver';
+import './Description.css'
 
 const Description = () => {
     
@@ -10,35 +11,9 @@ const Description = () => {
     }
     
     return(
-        <div>
-            Hi, my name is Sean and I'm a software engineer. I am passionate about web engineering! Please feel free to check out some of my projects. My hobbies include travel, movies, science-fiction, and biking &nbsp;<span id="smiley-face">🙂</span>
-            <br/>
-            <br/>
+        <div class="description-wrapper">
+            <span>Hi, my name is Sean and I'm a software engineer. I am passionate about web engineering! Please feel free to check out some of my projects. My hobbies include travel, movies, science-fiction, and biking &nbsp;<span id="smiley-face">🙂</span></span>
             <span onClick={downloadResume} id="resume-link"><span id="resume-no-space">Resume</span>&nbsp;<FileWordOutlined /></span>
-
-            <div class="emojis">
-                {/* <span id="emoji-one">
-                    🙂
-                </span>
-                
-                {/* <span id="emoji-two">
-                    😎
-                </span>
-                <span id="emoji-two">
-                    🥸
-                </span>
-                <span id="emoji-three">
-                    🤯
-                </span>
-                <span id="emoji-four">
-                    🇮🇳
-                </span>
-                <span id="emoji-four">
-                    ☕
-                </span> */}
-
-
-            </div>
         </div>
     )
 }

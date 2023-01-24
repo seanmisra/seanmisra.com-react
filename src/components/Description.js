fbe081352d38ae6d55f0b75515d01a78a59b88e3
Sean Misra <seanmisra30@gmail.com>
2023-01-24 07:29:24 -0500

    Card bio touch-ups

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FileWordOutlined } from '@ant-design/icons';
+import { FileWordOutlined, FilePdfOutlined} from '@ant-design/icons';
 import resume from '../resources/Sean_Misra_Resume_Aug_2022.docx'
 import { saveAs } from 'file-saver';
 import './Description.css'
@@ -13,7 +13,12 @@ const Description = () => {
     return(
         <div class="description-wrapper">
             <span>Hi, my name is Sean and I'm a software engineer. I am passionate about web engineering! Please feel free to check out some of my projects. My hobbies include travel, movies, science-fiction, and biking &nbsp;<span id="smiley-face">🙂</span></span>
-            <span onClick={downloadResume} id="resume-link"><span id="resume-no-space">Resume</span>&nbsp;<FileWordOutlined /></span>
+            <span id="hashtags">#angular #react #PHP #java </span>
+
+            <span onClick={downloadResume} id="resume-link">
+                <span id="resume-no-space">Resume</span> &nbsp;
+                <FilePdfOutlined />
+            </span>
         </div>
     )
 }

48559de15c823b66df1acc0ace43405050938312
Sean Misra <seanmisra30@gmail.com>
2023-01-28 09:06:36 -0500

    Content touch-ups

--- src/components/Message.js
+++ src/components/Message.js
@@ -7,16 +7,16 @@ const Message = () => {
         setTimeout(() => {
             const welcomeMessage = document.getElementById("welcome-message");
             welcomeMessage.style.opacity = 1;
-            setTimeout(() => {
-                welcomeMessage.style.opacity = 0;
-            }, 15000)
+            // setTimeout(() => {
+            //     welcomeMessage.style.opacity = 0;
+            // }, 15000)
         }, 5000)
       });
 
     return(
         <> 
         <div class="welcome-wrapper">
-            <span class="message-hidden" id="welcome-message">Thanks for visiting 🤝 Sample projects are above. Feel free to email me at <span id="email-message">seanmisra30@gmail.com</span> or ask any questions to the chatbot built with Dialogflow</span>
+            <span class="message-hidden" id="welcome-message">Thanks for visiting 🤝 sample projects are above. Feel free to email me at <span id="email-message">seanmisra30@gmail.com</span> or ask any questions to the chatbot built with Dialogflow</span>
         </div>
         </>
     )

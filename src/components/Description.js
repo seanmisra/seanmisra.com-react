2ddd3155df2c55f983df2a5ac01ff068e18806bf
Sean Misra <seanmisra30@gmail.com>
2023-01-29 23:54:22 -0500

    Downloadables

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FilePdfOutlined} from '@ant-design/icons';
+import { FileExcelOutlined} from '@ant-design/icons';
 import resume from '../resources/Sean_Misra_Resume_2023.pdf'
 import { saveAs } from 'file-saver';
 import './Description.css'
@@ -21,12 +22,19 @@ const Description = () => {
                     <span id="resume-no-space">Resume</span> &nbsp;
                     <FilePdfOutlined />
                 </span>
+
+                
             </span>
 
             <span class="resume-link" onClick={downloadResume} id="resume-link">
                 <span id="resume-no-space">Resume</span> &nbsp;
                 <FilePdfOutlined />
             </span>
+
+            {/* <span class="resume-link" onClick={downloadResume} id="resume-link">
+                <span id="resume-no-space">Skills</span> &nbsp;
+                <FileExcelOutlined />
+            </span> */}
         </div>
     )
 }
